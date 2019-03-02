8ccbb6f10143e9bd711310f9460de54eee2806d0
TheDonciuxX <bandym555@gmail.com>
2019-03-02 23:43:13 +0200

    Cleaned up Profject Files

--- generator/Properties/AssemblyInfo.cs
+++ generator/Properties/AssemblyInfo.cs
@@ -5,11 +5,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BrawlMapGen")]
-[assembly: AssemblyDescription("A generator which generates map images from arrays of strings and vector tiles.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MortalCode")]
-[assembly: AssemblyProduct("Brawl Stars Map Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +27,3 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
