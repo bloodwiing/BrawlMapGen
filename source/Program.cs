1d41ef9729cf75b6640c2a479135a65bfe9d1eac
TheDonciuxX <bandym555@gmail.com>
2019-05-20 15:23:18 +0300

    Update version

--- source/Program.cs
+++ source/Program.cs
@@ -17,7 +17,7 @@ namespace BMG
         {
             var fArgs = " " + string.Join(" ", args);
             string version = "1.8b";
-            string buildText = " [UNSTABLE BUILD 3]";
+            string buildText = " [UNSTABLE BUILD 4]";
             string oLoc = "options.json";
             string oStr = "";
             string oEnd = "";
